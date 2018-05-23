7c88519f2ff39de0853157feb319b55ba0fdbbb7
ASH IZADI <ash.izad@gmail.com>
2018-05-23 15:02:03 +1000

    update config to start application after deploy

--- startApp.sh
+++ startApp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export NODE_ENV=production
-export DB_PRD_HOST=dellstore2.us-west-2.rds.amazonaws.com
-export DB_PRD_USER=spidertutorial
-export DB_PRD_PASS=spidertutorial
+export DB_PRD_HOST=continuesdeployment.chkveepfyd9e.ap-southeast-2.rds.amazonaws.com
+export DB_PRD_USER=ashkan
+export DB_PRD_PASS=Password1
 export NODE_HOST=localhost
 export NODE_PORT=8080
 node /myapp/index.js&
