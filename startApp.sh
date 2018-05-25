64126984275459e1a2d141a5c325643aeb8f46d3
ash .izadi <ash.izad@gmail.com>
2018-05-25 07:16:45 +0100

    run nodejs as deamon

--- startApp.sh
+++ startApp.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-echo "call another bash file >>>"
-source startNode.sh
-runner
+
+export NODE_ENV=production
+export DB_PRD_HOST=continuesdeployment.chkveepfyd9e.ap-southeast-2.rds.amazonaws.com
+export DB_PRD_USER=ashkan
+export DB_PRD_PASS=Password1
+export NODE_HOST=localhost
+export NODE_PORT=8080
+echo "Starting application ..."
+node /myapp/index.js > /dev/null 2> /dev/null < /dev/null &
 exit 0
