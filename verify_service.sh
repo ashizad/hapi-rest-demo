afc96daaabd7655236c2a807d450718918c6e889
ash .izadi <ash.izad@gmail.com>
2018-05-25 07:48:01 +0100

    run nodejs as deamon

--- verify_service.sh
+++ verify_service.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 result=$(curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:8080/products/100)
-httpStatus = "200"
 
-if [[ "$result" == "$httpStatus" ]]; then
+if [ $result = "200" ]
+then
 	echo "Successfully pulled root page."
     exit 0
- fi
+else
+	echo "HTTP code is not matched"
+fi
   echo "Attempt to curl endpoint returned HTTP Code $result. Backing off and retrying."
   sleep 10
 done
