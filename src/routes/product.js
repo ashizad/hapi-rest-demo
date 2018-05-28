b0b67c9fa1cf666762bdb9883cc31c0f649fdac3
ash .izadi <ash.izad@gmail.com>
2018-05-28 03:39:08 +0100

    adding a new contoller into router

--- src/routes/product.js
+++ src/routes/product.js
@@ -19,6 +19,11 @@ module.exports = function () {
             method: 'GET',
             path: '/products',
             handler: productController.find
-        }
+        },
+	{
+		method:'GET',
+		path:'/products/category/{category},
+		handler:productController.findByCategory
+	}
     ];
 }();
